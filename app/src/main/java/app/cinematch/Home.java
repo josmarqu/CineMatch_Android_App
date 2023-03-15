ded17f986c7cf160d8ecc817eec93cbad806d367
Pedrojimvi <pedrojv1030@gmail.com>
2023-03-15 11:31:21 +0100

    Now it shows the photo and the name of the user

--- app/src/main/java/app/cinematch/Home.java
+++ app/src/main/java/app/cinematch/Home.java
@@ -1,12 +1,18 @@
 package app.cinematch;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -14,6 +20,10 @@ public class Home extends AppCompatActivity {
     Button mButtonCerrarSecion;
 
     FirebaseAuth mAuth;
+
+    TextView textViewRespuesta;
+
+    ImageView imgView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +33,32 @@ public class Home extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
 
         mButtonCerrarSecion = findViewById(R.id.btnCerrarSecion);
+
+        textViewRespuesta = findViewById(R.id.textViewBien);
+
+        imgView = findViewById(R.id.imageView);
+
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+        if (acct != null) {
+            String personName = acct.getDisplayName();
+            String personGivenName = acct.getGivenName();
+            String personFamilyName = acct.getFamilyName();
+            String personEmail = acct.getEmail();
+            String personId = acct.getId();
+            Uri personPhoto = acct.getPhotoUrl();
+
+            textViewRespuesta.setText("Bienvenido " + personGivenName);
+            Glide.with(this)
+                    .load(personPhoto)
+                    .into(imgView);
+
+            System.out.println(personName);
+            System.out.println(personGivenName);
+            System.out.println(personFamilyName);
+            System.out.println(personEmail);
+            System.out.println(personId);
+            System.out.println(personPhoto);
+        }
 
         mButtonCerrarSecion.setOnClickListener(new View.OnClickListener() {
             @Override
